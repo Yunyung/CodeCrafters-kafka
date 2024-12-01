5f5f7fed853a94e7f88ff85fc0aa201058d4c760
Yunyung <yungyung7654321@gmail.com>
2024-12-02 04:56:43 +0800

    Parse correlation id

--- app/server.go
+++ app/server.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"encoding/binary"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+var BUFFER_SIZE int = 1024
+const MESSAGE_SIZE int = 4 // bytes for message size
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -26,8 +29,15 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	
+	buffer := make([]byte, 1024)
 
-	conn.Read(make([]byte, 1024))
-	respond := []byte{0, 0, 0, 0, 0, 0, 0, 7}
+	conn.Read(buffer)
+	fmt.Printf("Received message %v (%d)", buffer[8:12], int32(binary.BigEndian.Uint32(buffer[8:12])))
+	respond := make([]byte, 8)
+	copy(respond, make([]byte, MESSAGE_SIZE)) // copy the first 4 bytes from the request[MESSAGE_SIZE]byte)
+	copy(respond[4:8], buffer[8:12])
 	conn.Write(respond)
+
+	conn.Close()
 }
