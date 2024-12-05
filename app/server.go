f61c6251731126501676cd2594b70c0b215a9210
Yunyung <yungyung7654321@gmail.com>
2024-12-05 15:08:22 +0800

    Handle APIVersions requests and Serial requests

--- app/server.go
+++ app/server.go
@@ -5,7 +5,18 @@ import (
 	"net"
 	"os"
 	"encoding/binary"
+	"bytes"
+	"unsafe"
 )
+
+type RequestHeader struct {
+	messageSize int32
+	requestApiKey int16
+	requestApiVersion int16
+	correlationId int32
+	clientID string
+	taggedFields string
+}
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
@@ -13,6 +24,48 @@ var _ = os.Exit
 var BUFFER_SIZE int = 1024
 const MESSAGE_SIZE int = 4 // bytes for message size
 
+type TaggedFields struct {
+	tagBuffer byte
+}
+type RespondHeader struct {
+	messageSize int32
+	correlationId int32
+}
+
+type ApiKey struct	{
+	apiKey int16
+	minVersion int16
+	maxVersion int16
+}
+type Respond struct {
+	respondHeader RespondHeader
+	errorCode int16
+	numApiKeys byte
+	apiKeys ApiKey
+	taggedFields TaggedFields
+	throttleTimeMs int32
+	taggedFields2 TaggedFields
+}
+
+func handleRequest(conn net.Conn) (RequestHeader, error) {
+	request := RequestHeader{}
+	err := binary.Read(conn, binary.BigEndian, &request.messageSize)
+	if err != nil {
+		fmt.Println("Error reading from connection!!!: ", err.Error())
+		return RequestHeader{}, err
+	}
+	binary.Read(conn, binary.BigEndian, &request.requestApiKey)
+	binary.Read(conn, binary.BigEndian, &request.requestApiVersion)
+	binary.Read(conn, binary.BigEndian, &request.correlationId)
+	fmt.Printf("Received request for message size (%d)\n", request.messageSize)
+	fmt.Printf("Received request for API key (%d)\n", request.requestApiKey)
+	fmt.Printf("Received request for API version (%d)\n", request.requestApiVersion)
+	fmt.Printf("Received correlation id (%d)\n", request.correlationId)
+	
+	conn.Read(make([]byte, 2048))
+
+	return request, nil
+}
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -29,23 +82,54 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	
-	buffer := make([]byte, 1024)
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			fmt.Println("Error closing listener: ", err.Error())
+			os.Exit(1)
+		}
+	}(l)
+	for {
+		var error_code int16 = 0
+		requestHeader, _ := handleRequest(conn)
+		if requestHeader.requestApiVersion < 0 || requestHeader.requestApiVersion > 4 {
+			error_code = 35
+		}
 
-	conn.Read(buffer)
-	request_api_version := int16(binary.BigEndian.Uint16(buffer[6:8]))
-	var error_code uint16 = 0
-	if request_api_version < 0 || request_api_version > 4 {
-		error_code = 35
+		fmt.Printf("RespondHeader len (%d)\n", unsafe.Sizeof(RespondHeader{}))
+		fmt.Printf("ApiKey len (%d)\n", unsafe.Sizeof(ApiKey{}))
+		fmt.Printf("TaggedFields len (%d)\n", unsafe.Sizeof(TaggedFields{}))
+		fmt.Printf("respond len (%d)\n", unsafe.Sizeof(Respond{}))
+
+
+		var respond Respond
+		respond.respondHeader.correlationId = requestHeader.correlationId
+		respond.errorCode = error_code
+		respond.numApiKeys = byte(2)
+		respond.apiKeys.apiKey = 18
+		respond.apiKeys.minVersion = 0
+		respond.apiKeys.maxVersion = 4
+		
+		buf := &bytes.Buffer{}
+		
+		// binary.Write(buf, binary.BigEndian, respond.respondHeader)
+		// binary.Write(buf, binary.BigEndian, respond.errorCode)
+		// binary.Write(buf, binary.BigEndian, respond.numApiKeys)
+		// binary.Write(buf, binary.BigEndian, respond.apiKeys)
+		// binary.Write(buf, binary.BigEndian, respond.taggedFields)
+		// binary.Write(buf, binary.BigEndian, respond.throttleTimeMs)
+		// binary.Write(buf, binary.BigEndian, respond.taggedFields)
+        // // Correctly calculate message size after writing fields.
+        // respond.respondHeader.messageSize = int32(buf.Len())
+        // binary.BigEndian.PutUint32(buf.Bytes()[:4], uint32(respond.respondHeader.messageSize) - 4)
+		binary.Write(buf, binary.BigEndian, respond)
+		respond.respondHeader.messageSize = int32(buf.Len())
+        binary.BigEndian.PutUint32(buf.Bytes()[:4], uint32(respond.respondHeader.messageSize) - 4)
+
+
+		fmt.Printf("Afer: Sizeof respond: %d, Sizeof buf: %d, Len of buf: %d\n", unsafe.Sizeof(respond), unsafe.Sizeof(buf), buf.Len() )
+		fmt.Println("Buffer as bytes:", buf.Bytes())
+
+		conn.Write(buf.Bytes())
 	}
-	fmt.Printf("Received request for API version (%d)\n", request_api_version)
-	fmt.Printf("Received message %v (%d)", buffer[8:12], int32(binary.BigEndian.Uint32(buffer[8:12])))
-	respond := make([]byte, 24)
-	copy(respond, make([]byte, MESSAGE_SIZE)) // copy the first 4 bytes from the request[MESSAGE_SIZE]byte)
-	copy(respond[4:8], buffer[8:12]) // correlation id
-	//copy(respond[8:12], error_code)
-	binary.BigEndian.PutUint16(respond[8:10], error_code)
-	conn.Write(respond)
-
-	conn.Close()
 }
